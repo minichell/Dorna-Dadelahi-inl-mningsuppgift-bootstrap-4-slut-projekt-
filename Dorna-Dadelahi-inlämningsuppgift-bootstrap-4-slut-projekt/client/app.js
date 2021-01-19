1addb72e25c4a1ea8f21d70c155b77bcd8e2a7a2
minichell <54796879+minichell@users.noreply.github.com>
2021-01-19 10:57:20 +0100

    Update app.js

--- Dorna-Dadelahi-inlämningsuppgift-bootstrap-4-slut-projekt/client/app.js
+++ Dorna-Dadelahi-inlämningsuppgift-bootstrap-4-slut-projekt/client/app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const 
+const InfoModule = require ('./InfoModule')
 const app = express()
 const port = 3000
 
+const clientDir = __dirname + "//client//"
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use (express.static(clientDir))
+
+app.get('/', (req,res) => {
+    res.render("pages/contact.html")
+})
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
