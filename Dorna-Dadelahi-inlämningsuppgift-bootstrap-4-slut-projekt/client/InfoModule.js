1468a56e5139f5f64db89e3b5a67f9e48699af23
minichell <54796879+minichell@users.noreply.github.com>
2021-01-19 10:46:22 +0100

    express

---
+++ Dorna-Dadelahi-inlämningsuppgift-bootstrap-4-slut-projekt/client/InfoModule.js
@@ -0,0 +1,21 @@
+const mongoose = require ('mongoose')
+
+const infoSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    phone: string
+})
+
+const Info = mongoose.model ('Info', infoSchema);
+
+exports.createInfo = (first, last, email, phone) => {
+    var info = new Info({
+        firstname: first,
+        lastname: last,
+        email: email,
+        phone: phone
+    })
+
+    return info
+}
