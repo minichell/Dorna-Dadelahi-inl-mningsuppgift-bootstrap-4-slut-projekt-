ec54de20da274b397465f2306ead5b3df11c9208
minichell <54796879+minichell@users.noreply.github.com>
2021-01-24 16:18:17 +0100

    :/

--- Dorna-Dadelahi-inlämningsuppgift-bootstrap-4-slut-projekt/client/app.js
+++ Dorna-Dadelahi-inlämningsuppgift-bootstrap-4-slut-projekt/client/app.js
@@ -4,15 +4,22 @@ const InfoModule = require ('./InfoModule')
 const app = express()
 const port = 3000
 
-const clientDir = __dirname + "//client//"
+const clientDir = __dirname + "/html/"
+const cssDir = __dirname + "/css/"
+//console.log ( __dirname )
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use (express.static(clientDir))
 
+app.get('/', (req, res) =>{
+    res.render(clientDir + "index.html")
+    res.render(cssDir + "bootstrap.min.css")
+})
+
 app.get('/contact', async (req, res) => {
     let contact = await InfoModule.GetAllInfo();
-    res.render("pages/contact.html", {info: contact})
+    res.render("./html/contact.html", {info: contact})
 })
 
 app.post ('/contact', async (req,res) => {
@@ -21,8 +28,8 @@ app.post ('/contact', async (req,res) => {
 
     var infos = await InfoModule.GetAllInfo();
 
-    res.render('pages/conact.html',  {info: contact});
+    res.render('./html/conact.html',  {info: contact});
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
